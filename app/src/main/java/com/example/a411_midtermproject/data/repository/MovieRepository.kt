cd0cfb48b5780572582bb88e802fbe54a5cfd272
ELM <31elopez@gmail.com>
2025-10-13 00:46:25 -0700

    Added some more movies

--- app/src/main/java/com/example/a411_midtermproject/data/repository/MovieRepository.kt
+++ app/src/main/java/com/example/a411_midtermproject/data/repository/MovieRepository.kt
@@ -7,6 +7,9 @@ object MovieRepository {
     val movies = listOf(
         Movie(1, "Inception", "A thief who steals secrets through dreams.", 8.8, R.drawable.ic_launcher_foreground),
         Movie(2, "Interstellar", "Explorers travel through a wormhole in space.", 8.6, R.drawable.ic_launcher_foreground),
-        Movie(3, "The Dark Knight", "Batman faces the Joker in Gotham.", 9.0, R.drawable.ic_launcher_foreground)
+        Movie(3, "The Dark Knight", "Batman faces the Joker in Gotham.", 9.0, R.drawable.ic_launcher_foreground),
+        Movie(4, "Chappie", "Police robot becomes sentient.", 7.0, R.drawable.ic_launcher_foreground),
+        Movie(5, "Speed Racer", "Car goes fast.", 9.0, R.drawable.ic_launcher_foreground),
+        Movie(6, "Pokemon 2000", "Pokemon trainer goes on an adventure with Lugia.", 9.0, R.drawable.ic_launcher_foreground)
     )
 }
